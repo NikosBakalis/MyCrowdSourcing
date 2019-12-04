d88961855bd0bec11ac02a532b192d5234d7a618
NikosBakalis <bakalis.nikolas@gmail.com>
2019-12-04 23:23:29 +0200

    Both encryption and decryption functions added! Also, MySQL code changed so as to accepts the encrypted ID for each user.

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,5 +1,21 @@
 <?php
 
+function encrypt($decrepted_string, $encrytion_key){ //This one right here is the encryptment function we use on line 77 to encrypt users ID.
+  $ciphering = "AES-128-CTR";
+  $options = 0;
+  $encryption_iv = '1234567891011121';
+  $string = openssl_encrypt($decrepted_string, $ciphering, $encrytion_key, $options, $encryption_iv);
+  return $string;
+}
+
+function decrypt($encrypted_string, $decryption_key){ //This one right here is the decryptment function and we don't actually use it yet.
+  $ciphering = "AES-128-CTR";
+  $options = 0;
+  $decryption_iv = '1234567891011121';
+  $string = openssl_decrypt ($encrypted_string, $ciphering, $decryption_key, $options, $decryption_iv);
+  return $string;
+}
+
 /* This one right here we will chech if the user entered
 this sections using the submit button
 If that's true then we will let him continue
@@ -44,22 +60,23 @@ if (isset($_POST['signup_submit'])) {
     else { //This one right here is called if the sql statement is working properly and executes it.
       mysqli_stmt_bind_param($stmt, "s", $email);
       mysqli_stmt_execute($stmt);
-      mysqli_stmt_store_result($stmt); //This one right here fecthes info from the database and thats why we dont use it again down after line 63.
+      mysqli_stmt_store_result($stmt); //This one right here fecthes info from the database and thats why we dont use it again down after line 80.
       $resultCheck = mysqli_stmt_num_rows($stmt);
       if ($resultCheck > 0) {
         header("Location: ../signup.php?error=user_already_taken&firstname=".$firstname."&lastname=".$lastname."&username=".$username);
         exit();
       }
       else { //This one right here checks if the email of the user is already used in the database again.
-        $sql = "INSERT INTO users(firstname, lastname, username, email, pwd) VALUES(?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO users(id, firstname, lastname, username, email, pwd) VALUES(?, ?, ?, ?, ?, ?)";
         $stmt = mysqli_stmt_init($connection);
         if (!mysqli_stmt_prepare($stmt, $sql)) { //This one right here will check if the sql statement above working properly again.
           header("Location: ../signup.php?error=sql_error");
           exit();
         }
         else { //This one right here is called if the sql statement is working properly and executes it again.
+          $encryptedID = encrypt($email, $password);
           $hashedPassword = password_hash($password, PASSWORD_DEFAULT); //BUT FIRST!!! This one right here hashes the password on bcrypt Andrea cuz its fucking awesome and way better than SHA-256 :P :P.
-          mysqli_stmt_bind_param($stmt, "sssss", $firstname, $lastname, $username, $email, $hashedPassword);
+          mysqli_stmt_bind_param($stmt, "ssssss", $encryptedID, $firstname, $lastname, $username, $email, $hashedPassword);
           mysqli_stmt_execute($stmt);
           header("Location: ../signup.php?signup=success");
           exit();
@@ -70,7 +87,7 @@ if (isset($_POST['signup_submit'])) {
   mysqli_stmt_close($stmt); //This one right here closes the statement.
   mysqi_close($connection); //This one right here closes the connection.
 }
-else { //This one right here sent the curious user back to home when he tries to enter the include page in other way that from the button I mentioned on lines 3-4-5-6.
+else { //This one right here sent the curious user back to home when he tries to enter the include page in other way that from the button I mentioned on lines 19-20-21-22.
   header("Location: ../signup.php");
   exit();
 }
