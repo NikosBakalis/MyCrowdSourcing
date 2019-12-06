8bdd75ef2c47dd3dba204e01e4365f009e0afe40
NikosBakalis <bakalis.nikolas@gmail.com>
2019-12-07 01:01:55 +0200

    Admin basic checks completed!

--- admin.php
+++ admin.php
@@ -2,7 +2,12 @@
   include 'header.php';
 ?>
 
-<?php echo $_SESSION['userID'] ?>
+<?php
+
+  echo $_SESSION['userID'];
+  echo date(' d/m/Y H:i:s', 1464900582701 / 1000);
+
+?>
 
 <?php
   include 'footer.php';
