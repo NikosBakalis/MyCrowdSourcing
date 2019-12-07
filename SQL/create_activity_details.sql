e078d917074a43c07ef72c166de1b451652c3e0c
NikosBakalis <bakalis.nikolas@gmail.com>
2019-12-07 17:09:55 +0200

    Primary key fixed!

--- SQL/create_activity_details.sql
+++ SQL/create_activity_details.sql
@@ -1,11 +1,10 @@
 CREATE TABLE activity_details(
-    auto_inc INT AUTO_INCREMENT NOT NULL,
     userID VARCHAR(200) NOT NULL,
     timestamp_l DATETIME NOT NULL,
     timestamp_a DATETIME NOT NULL,
     type VARCHAR(12) NOT NULL,
     confidence TINYINT NOT NULL,
-    PRIMARY KEY(auto_inc),
+    PRIMARY KEY(userID, timestamp_l, timestamp_a, type),
     FOREIGN KEY(userID, timestamp_l, timestamp_a) REFERENCES activity(userID, timestamp_l, timestamp_a)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
