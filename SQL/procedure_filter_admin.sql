ceee74a2d5d46e87cd6261a330d5147720c1b7c9
andreasceid <ankaratzas@ceid.upatras.gr>
2019-12-10 19:55:24 +0200

    Update v.2 Procedure

--- SQL/procedure_filter_admin.sql
+++ SQL/procedure_filter_admin.sql
@@ -1,3 +1,5 @@
+DROP PROCEDURE IF EXISTS filter_json;
+
 DELIMITER $$
  
 CREATE PROCEDURE filter_json(IN  activity_stream VARCHAR(255))
@@ -5,15 +7,21 @@ BEGIN
     DECLARE activities VARCHAR(255);
     DECLARE tmp VARCHAR(255);
     SELECT activity_stream INTO activities;
+    DROP TABLE IF EXISTS activity_stream;
+    CREATE TABLE activity_table(
+        activity_type VARCHAR(20)
+    );
     WHILE (activities > '') DO
         SELECT SUBSTRING_INDEX(activities, '.', 1) INTO tmp;
         SELECT tmp;
+        INSERT INTO activity_table VALUES (tmp);
         SELECT REPLACE(activities, tmp, ' ') INTO activities;
         SELECT TRIM(activities) INTO activities;
         SELECT SUBSTR(activities, 2) INTO activities;
         SELECT TRIM(activities) INTO activities;
         SELECT activities;
     END WHILE;
+    SELECT * FROM activity_table;
 END$$
 
 DELIMITER ;
