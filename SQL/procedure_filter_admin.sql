3d11f74d9a37e05edb5b0205d20a79023a42ae60
NikosBakalis <bakalis.nikolas@gmail.com>
2019-12-10 20:21:05 +0200

    Procedure READY!

--- SQL/procedure_filter_admin.sql
+++ SQL/procedure_filter_admin.sql
@@ -2,7 +2,7 @@ DROP PROCEDURE IF EXISTS filter_json;
 
 DELIMITER $$
 
-CREATE PROCEDURE filter_json(IN  activity_stream VARCHAR(255))
+CREATE PROCEDURE filter_json(IN time_begin DATETIME, IN time_end DATETIME, IN  activity_stream VARCHAR(255))
 BEGIN
     DECLARE activities VARCHAR(255);
     DECLARE tmp VARCHAR(255);
@@ -22,8 +22,19 @@ BEGIN
         /*SELECT activities;*/
     END WHILE;
     SELECT * FROM activity_table;
+    select * from location
+    inner join activity
+    on location.userID = activity.userID
+    and location.timestamp_l = activity.timestamp_l
+    inner join activity_details
+    on activity.userID = activity_details.userID
+    and activity.timestamp_l = activity_details.timestamp_l
+    and activity.timestamp_a = activity_details.timestamp_a
+    where activity_details.type in (select * from activity_table)
+    AND (activity_details.timestamp_l BETWEEN time_begin AND time_end);
+
 END$$
 
 DELIMITER ;
 
-CALL filter_json("WALKING.STILL.DRIVING.UNKNOWN");
+CALL filter_json('2018-05-03 11:53:49', '2018-05-03 14:36:23', 'WALKING.STILL.DRIVING.UNKNOWN');
