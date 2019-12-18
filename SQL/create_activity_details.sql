5ef7134281b52e36147d5c3faa474df646190659
NikosBakalis <bakalis.nikolas@gmail.com>
2019-12-18 17:34:13 +0200

    type VARCHAR(20) instead of 12 for bigger worlds!

--- SQL/create_activity_details.sql
+++ SQL/create_activity_details.sql
@@ -2,7 +2,7 @@ CREATE TABLE activity_details(
     userID VARCHAR(200) NOT NULL,
     timestamp_l DATETIME NOT NULL,
     timestamp_a DATETIME NOT NULL,
-    type VARCHAR(12) NOT NULL,
+    type VARCHAR(20) NOT NULL,
     confidence TINYINT NOT NULL,
     PRIMARY KEY(userID, timestamp_l, timestamp_a, type),
     FOREIGN KEY(userID, timestamp_l, timestamp_a) REFERENCES activity(userID, timestamp_l, timestamp_a)
