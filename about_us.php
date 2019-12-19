a23f02218904e87b63b17b95d769ddbf028cc4ad
NikosBakalis <bakalis.nikolas@gmail.com>
2019-12-19 03:38:20 +0200

    Some basic files uploaded! TODO: Populate them.

---
+++ about_us.php
@@ -0,0 +1,11 @@
+<?php
+  include 'header.php';
+?>
+
+
+<!-- This one right here will be the about us page so as to help the user learn about us -->
+
+
+<?php
+  include 'footer.php';
+?>
---
+++ profile.php
@@ -0,0 +1,11 @@
+<?php
+  include 'header.php';
+?>
+
+
+<!-- This one right here will be the profile of the user -->
+
+
+<?php
+  include 'footer.php';
+?>
