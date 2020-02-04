bb1231a32d062f08af2b9d076d0693353351dc8e
NikosBakalis <bakalis.nikolas@gmail.com>
2020-02-04 23:22:27 +0200

    Fixes for background processing!

--- includes/from_mysql_to_heatmap.inc.php
+++ includes/from_mysql_to_heatmap.inc.php
@@ -1,8 +1,9 @@
 <?php
 
-$targetPath = "../uploads/".basename($_FILES["upload_file"]["name"]);
-echo $targetPath;
-console.log($targetPath);
-move_uploaded_file($_FILES["upload_file"]["tmp_name"], $targetPath);
+// $targetPath = "../uploads/".basename($_FILES["upload_file"]["name"]);
+// echo $targetPath;
+// console.log($targetPath);
+// move_uploaded_file($_FILES["upload_file"]["tmp_name"], $targetPath);
+echo "string";
 
 ?>
