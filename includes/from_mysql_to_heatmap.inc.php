e1a30cf840b68f32a1bc59f8559f72f7c6820b77
NikosBakalis <bakalis.nikolas@gmail.com>
2020-02-05 18:44:02 +0200

    Test file, maybe use later!

--- includes/from_mysql_to_heatmap.inc.php
+++ includes/from_mysql_to_heatmap.inc.php
@@ -1,9 +1,5 @@
 <?php
 
-// $targetPath = "../uploads/".basename($_FILES["upload_file"]["name"]);
-// echo $targetPath;
-// console.log($targetPath);
-// move_uploaded_file($_FILES["upload_file"]["tmp_name"], $targetPath);
-echo "string";
+echo "Basic test";
 
 ?>
