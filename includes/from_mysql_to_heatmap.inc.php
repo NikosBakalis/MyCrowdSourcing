5f386726ae96e5289b3b3a2ee49090dac8bd16f8
NikosBakalis <bakalis.nikolas@gmail.com>
2020-02-07 19:38:40 +0200

    Php for heatmaps!!!

--- includes/from_mysql_to_heatmap.inc.php
+++ includes/from_mysql_to_heatmap.inc.php
@@ -1,5 +1,35 @@
 <?php
 
-echo "Basic test";
+session_start();
+
+include 'dbhandler.inc.php';
+
+$big_array = array();
+
+$userID = $_SESSION['userID'];
+$sql = "SELECT * FROM location WHERE userID = '$userID'";
+$stmt = mysqli_stmt_init($connection);
+if (!mysqli_stmt_prepare($stmt, $sql)) { //This one right here will check if the sql statement above working properly.
+  echo "Connection failed!";
+  exit();
+}
+else{
+  $result = mysqli_query($connection, $sql);
+  $resultCheck = mysqli_num_rows($result);
+
+  if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      // echo $row['latitude'] . '<br>';
+      $float_latitide = floatval($row['latitude']);
+      $float_longitude = floatval($row['longitude']);
+      $lat_lon_opa_array = array();
+      array_push($lat_lon_opa_array, $float_latitide, $float_longitude);
+      array_push($big_array, $lat_lon_opa_array);
+      unset($lat_lon_opa_array);
+    }
+  }
+}
+
+echo json_encode($big_array);
 
 ?>
