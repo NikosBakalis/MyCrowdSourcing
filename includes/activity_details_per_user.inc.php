a2b7b6a39069b18357a825b84cd6b14bfd98f0bd
NikosBakalis <bakalis.nikolas@gmail.com>
2020-02-17 13:50:36 +0200

    File fixed runs with no errors!

--- includes/activity_details_per_user.inc.php
+++ includes/activity_details_per_user.inc.php
@@ -2,49 +2,42 @@
 
 include 'dbhandler.inc.php';
 
-if (isset($_POST['type_of_activity'])) {
-  $name = $_POST['type_of_activity'];
-  // echo $name;
+if (isset($_POST['type_of_activity'])) { // This one right here checks if we are here from the 'type_of_activity' button in frontend.
+  $name = $_POST['type_of_activity']; // This one right here stores the value that the user inserted into a variable.
 
-  $error_empty = false;
+  // $error_empty = false;
 
-  if (empty($name)) {
-    echo "<span class='form-error'>Fill in all fields!</span>";
-    $error_empty = true;
-  } else {
-    echo "<span class='form-success'>Success!</span>";
-  }
-
-  if (!empty($name)) {
-    $sql_1 = "SELECT * FROM activity_details";
-    $sql_2 = "SELECT * FROM activity_details WHERE type = '$name'";
+  if (empty($name)) { // This one right here checks if the input of the user is empty.
+    echo "<span class='form-error'>Fill in all fields!</span>"; // This one right here echo this message if the input is empty.
+    // $error_empty = true;
+  } elseif (!empty($name)) { // This one right here checjs if the input of the user is not empty.
+    // echo "<span class='form-success'>Success!</span>"; // This one right here echo this message if the input is not empty.
+    $sql_1 = "SELECT * FROM activity_details"; // This one right here selects everything from activity_details table. I want only the count but I will retrieve it with mysqli_num_rows() function.
+    $sql_2 = "SELECT * FROM activity_details WHERE type = '$name'"; // This one right here selects everything from activity_details table where type is something that the admin choose in frontend. I want only the count but I will retrieve it with mysqli_num_rows() function.
     $stmt = mysqli_stmt_init($connection);
     if (!mysqli_stmt_prepare($stmt, $sql_1) || !mysqli_stmt_prepare($stmt, $sql_2)) { //This one right here will check if the sql statement above working properly.
       echo "Connection failed!";
       exit();
     }
     else{
-      $result_1 = mysqli_query($connection, $sql_1);
-      $resultCheck_1 = mysqli_num_rows($result_1);
-      // echo $resultCheck_1 . '<br>';
-      $result_2 = mysqli_query($connection, $sql_2);
-      $resultCheck_2 = mysqli_num_rows($result_2);
-      // echo $resultCheck_2 . '<br>';
-      echo ($resultCheck_2 / $resultCheck_1) * 100 . "%";
-
-      // if ($resultCheck_2 > 0) {
-      //   while ($row = mysqli_fetch_assoc($result_2)) {
-      //     // echo $row['latitude'] . '<br>';
-      //     // echo $row['longitude'] . '<br>';
-      //     // $float_latitide = floatval($row['latitude']);
-      //   }
-      // }
+      $result_1 = mysqli_query($connection, $sql_1); // This one right here stores the query into a variable.
+      $resultCheck_1 = mysqli_num_rows($result_1); // This one right here stores the mysqli_num_rows() into a variable.
+      $result_2 = mysqli_query($connection, $sql_2); // This one right here stores the query into a variable.
+      $resultCheck_2 = mysqli_num_rows($result_2); // This one right here stores the mysqli_num_rows() into a variable.
+      echo ($resultCheck_2 / $resultCheck_1) * 100 . "%"; // This one right here echos the activity_percentage of the specific activity the user entered before.
     }
   }
 
   // echo $float_latitide;
-} else {
+} else { //This one right here sent the curious user back to home when he tries to enter the include page in other way that from the button I mentioned on line 5.
   echo "There was an error!";
 }
 
 ?>
+
+<!-- <script>
+  var error_empty = "<?php echo $error_empty; ?>";
+  if(error_empty == true){
+    $("#type_of_activity").addClass("input-error");
+  }
+</script> -->
