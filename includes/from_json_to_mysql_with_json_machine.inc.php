0d4c0e8060335c76b79327421a9291a2d165a394
NikosBakalis <bakalis.nikolas@gmail.com>
2020-03-04 04:23:25 +0200

    TODO: Finish the multiple upload!!!

--- includes/from_json_to_mysql_with_json_machine.inc.php
+++ includes/from_json_to_mysql_with_json_machine.inc.php
@@ -1,9 +1,5 @@
 <?php
 
-$myfile = fopen("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt", "r") or die("Unable to open file!"); // This one right here opes the file we creted on uploads.inc.php.
-$_SESSION['userID'] = file_get_contents('C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt'); // This one right here gets the contents of the file.
-fclose($myfile); // This one right here closes the file we opened.
-
 set_time_limit (0);
 ini_set('memory_limit', '-1');
 require_once('C:/xampp/htdocs/MyCrowdSourcing/includes/json_machine/JsonMachine.php');
@@ -31,53 +27,80 @@ while(($files = readdir($resource)) != false) { //This one right here executes i
   if ($files != '.' && $files != '..' && $files != 'current_userID.txt' && $files != 'empty_text.txt') { //This one right here excludes the "." and the ".." files from the folder searching.
     require 'C:/xampp/htdocs/MyCrowdSourcing/includes/dbhandler.inc.php';
 
-    $locations = \JsonMachine\JsonMachine::fromFile('C:/xampp/htdocs/MyCrowdSourcing/uploads/'.$files, "/locations");
+    $myfile = fopen("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt", "r") or die("Unable to open file!"); // This one right here opes the file we creted on uploads.inc.php.
+    if ($myfile) {
+      while (($line = fgets($myfile)) !== false) {
+        // process the line read.
+        $pieces = explode(" : ", $line);
+        $_SESSION['userID'] = $pieces[0];
+        $json_name = $pieces[1];
+        $json_name = trim(preg_replace("/\s\s+/", "", $json_name));
+        echo $_SESSION['userID']." : ".$json_name;
 
-      foreach ($locations as $key => $location_values) {
-        $sql1 = "INSERT INTO location(userID, timestamp_l, latitude, longitude, accuracy, heading, vertical_accuracy, velocity, altitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt1 = mysqli_stmt_init($connection);
-        if (!mysqli_stmt_prepare($stmt1, $sql1)) { //This one right here will check if the sql statement above working properly.
-          echo "Connection failed!";
-          exit();
-        }
-        else {
-          //This one right below help us change googles json data while parsing so as to use them later.
-          $thisTimestampMs_l = date('Y-m-d H:i:s', $location_values['timestampMs'] / 1000);
-          $thisLatitudeE7 = $location_values['latitudeE7'] / pow(10, 7);
-          $thislongitudeE7 = $location_values['longitudeE7'] / pow(10, 7);
-          if (getDistanceBetweenPointsNew(38.230462, 21.753150, $thisLatitudeE7, $thislongitudeE7) < 10.0) { //This one right here is the use of the function we created on line 11.
-            mysqli_stmt_bind_param($stmt1, "ssddiiiii", $_SESSION['userID'], $thisTimestampMs_l, $thisLatitudeE7, $thislongitudeE7, $location_values['accuracy'], $location_values['heading'], $location_values['verticalAccuracy'], $location_values['velocity'], $location_values['altitude']);
-            mysqli_stmt_execute($stmt1);
-            if (isset($location_values['activity'])){
-              foreach ($location_values['activity'] as $keys => $activity_values) {
-                $sql2 = "INSERT INTO activity(userID, timestamp_l, timestamp_a) VALUES (?, ?, ?)";
-                $stmt2 = mysqli_stmt_init($connection);
-                if (!mysqli_stmt_prepare($stmt2, $sql2)) { //This one right here will check if the sql statement above working properly.
-                  echo "Connection failed!";
-                  exit();
-                }
-                else {
-                  $thisTimestampMs_a = date('Y-m-d H:i:s', $activity_values['timestampMs'] / 1000); //This one right here help us change googles json data while parsing so as to use them later.
-                    mysqli_stmt_bind_param($stmt2, "sss", $_SESSION['userID'], $thisTimestampMs_l, $thisTimestampMs_a);
-                    mysqli_stmt_execute($stmt2);
-                  foreach ($activity_values['activity'] as $keyss => $activity_details_values) {
-                    $sql3 = "INSERT INTO activity_details(userID, timestamp_l, timestamp_a, type, confidence) VALUES (?, ?, ?, ?, ?)";
-                    $stmt3 = mysqli_stmt_init($connection);
-                    if (!mysqli_stmt_prepare($stmt3, $sql3)) { //This one right here will check if the sql statement above working properly.
+        $write = $json_name;
+        $myfile = file_put_contents('C:/xampp/htdocs/MyCrowdSourcing/uploads/empty_text.txt', $write.PHP_EOL , FILE_APPEND | LOCK_EX);
+
+        if ($files == $json_name && substr_count(file_get_contents("C:/xampp/htdocs/MyCrowdSourcing/uploads/empty_text.txt"), $json_name) <= 1) {
+          echo "LEEEEEEEEEEEEEEEEEEEEEEEEEEEEL";
+          $locations = \JsonMachine\JsonMachine::fromFile('C:/xampp/htdocs/MyCrowdSourcing/uploads/'.$files, "/locations");
+
+          foreach ($locations as $key => $location_values) {
+            $sql1 = "INSERT INTO location(userID, timestamp_l, latitude, longitude, accuracy, heading, vertical_accuracy, velocity, altitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $stmt1 = mysqli_stmt_init($connection);
+            if (!mysqli_stmt_prepare($stmt1, $sql1)) { //This one right here will check if the sql statement above working properly.
+              echo "Connection failed!";
+              exit();
+            }
+            else {
+              //This one right below help us change googles json data while parsing so as to use them later.
+              $thisTimestampMs_l = date('Y-m-d H:i:s', $location_values['timestampMs'] / 1000);
+              $thisLatitudeE7 = $location_values['latitudeE7'] / pow(10, 7);
+              $thislongitudeE7 = $location_values['longitudeE7'] / pow(10, 7);
+              if (getDistanceBetweenPointsNew(38.230462, 21.753150, $thisLatitudeE7, $thislongitudeE7) < 10.0) { //This one right here is the use of the function we created on line 11.
+                mysqli_stmt_bind_param($stmt1, "ssddiiiii", $_SESSION['userID'], $thisTimestampMs_l, $thisLatitudeE7, $thislongitudeE7, $location_values['accuracy'], $location_values['heading'], $location_values['verticalAccuracy'], $location_values['velocity'], $location_values['altitude']);
+                mysqli_stmt_execute($stmt1);
+                if (isset($location_values['activity'])){
+                  foreach ($location_values['activity'] as $keys => $activity_values) {
+                    $sql2 = "INSERT INTO activity(userID, timestamp_l, timestamp_a) VALUES (?, ?, ?)";
+                    $stmt2 = mysqli_stmt_init($connection);
+                    if (!mysqli_stmt_prepare($stmt2, $sql2)) { //This one right here will check if the sql statement above working properly.
                       echo "Connection failed!";
                       exit();
                     }
                     else {
-                      mysqli_stmt_bind_param($stmt3, "ssssi", $_SESSION['userID'], $thisTimestampMs_l, $thisTimestampMs_a, $activity_details_values['type'], $activity_details_values['confidence']);
-                      mysqli_stmt_execute($stmt3);
+                      $thisTimestampMs_a = date('Y-m-d H:i:s', $activity_values['timestampMs'] / 1000); //This one right here help us change googles json data while parsing so as to use them later.
+                      mysqli_stmt_bind_param($stmt2, "sss", $_SESSION['userID'], $thisTimestampMs_l, $thisTimestampMs_a);
+                      mysqli_stmt_execute($stmt2);
+                      foreach ($activity_values['activity'] as $keyss => $activity_details_values) {
+                        $sql3 = "INSERT INTO activity_details(userID, timestamp_l, timestamp_a, type, confidence) VALUES (?, ?, ?, ?, ?)";
+                        $stmt3 = mysqli_stmt_init($connection);
+                        if (!mysqli_stmt_prepare($stmt3, $sql3)) { //This one right here will check if the sql statement above working properly.
+                          echo "Connection failed!";
+                          exit();
+                        }
+                        else {
+                          mysqli_stmt_bind_param($stmt3, "ssssi", $_SESSION['userID'], $thisTimestampMs_l, $thisTimestampMs_a, $activity_details_values['type'], $activity_details_values['confidence']);
+                          mysqli_stmt_execute($stmt3);
+                        } //else closes.
+                      } //foreach closes.
                     } //else closes.
                   } //foreach closes.
-                } //else closes.
-              } //foreach closes.
-            } //if closes.
-          } //functions if closes.
-        } //else closes.
-      } //foreach closes.
+                } //if closes.
+              } //functions if closes.
+            } //else closes.
+          } //foreach closes.
+        } //if closes.
+      } //while closes.
+    fclose($myfile);
+    } else {
+      // error opening the file.
+    }
+    //$_SESSION['userID'] = file_get_contents('C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt'); // This one right here gets the contents of the file.
+
+    //fclose($myfile); // This one right here closes the file we opened.
+
+    file_put_contents("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt", ""); // This one right here clears the contents of the current_userID.txt file.
+    file_put_contents("C:/xampp/htdocs/MyCrowdSourcing/uploads/empty_text.txt", ""); // This one right here clears the contents of the empty_text.txt file.
     unlink('C:/xampp/htdocs/MyCrowdSourcing/uploads/'.$files); //This one right here deletes the file we just parsed from the directory it has been uploaded.
   } //if closes.
 } //when closes.
--- includes/uploads.inc.php
+++ includes/uploads.inc.php
@@ -3,7 +3,7 @@
 session_start();
 
 // This one right here is to create a function that executes some art of my code in the background.
-function execInBackground($cmd, $current_userID) {
+function execInBackground($cmd, $current_userID, $json_name) {
   $descriptorspec = array(
      0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
      1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
@@ -11,16 +11,39 @@ function execInBackground($cmd, $current_userID) {
   );
   $pipes = array(); // This one right here is an empty array.
   if(substr(php_uname(), 0, 7) == "Windows"){ // This one right here checks if we are on a suprime enviroment!!!
-    $process = /*pclose(*/proc_open("start /B ". $cmd, $descriptorspec, $pipes)/*)*/; // This one right here needs THREE arguments to execure!!!
+    $process = /*pclose(*/proc_open("start ". $cmd, $descriptorspec, $pipes)/*)*/; // This one right here needs THREE arguments to execure!!!
   }
   else { // This one right here checks if we use trollinux.
     exec($cmd . " > /dev/null &"); // This one right here is like proc_open.
   }
+  // echo filesize("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt");
+  // echo file_get_contents("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt");
 
-  $myfile = fopen("../uploads/current_userID.txt", "w") or die("Unable to open file!"); //This one right here creates a file in uploads folder with name "current_userID.txt"
-  $txt = $_SESSION['userID']; // This one right here stores the value of the userID to a variable with name $txt.
-  fwrite($myfile, $txt); // This one right here writes in the file the userID of the current user.
-  fclose($myfile); // This one right here closes the file.
+
+  // if($old_content = file_get_contents("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt")){
+  //   $myfile = fopen("../uploads/current_userID.txt", "w") or die("Unable to open file!"); //This one right here creates a file in uploads folder with name "current_userID.txt"
+  //   $txt = $_SESSION['userID']; // This one right here stores the value of the userID to a variable with name $txt.
+  //   // echo "11";
+  //   // $old_content = file_get_contents("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt");
+  //   // echo $old_content."<br>";
+  //   // fwrite($myfile, $old_content."<br>"); // This one right here writes in the file the userID of the current user.
+  //   // fwrite($myfile, $txt." : ".$json_name);
+  //   $input = $txt." : ".$json_name;
+  //   // echo $old_content."<br>";
+  //   echo $txt." : ".$json_name;
+  //   file_put_contents("C:/xampp/htdocs/MyCrowdSourcing/uploads/current_userID.txt", $input, FILE_APPEND);
+  // }
+  // else {
+  //   $myfile = fopen("../uploads/current_userID.txt", "w") or die("Unable to open file!"); //This one right here creates a file in uploads folder with name "current_userID.txt"
+  //   $txt = $_SESSION['userID']; // This one right here stores the value of the userID to a variable with name $txt.
+  //   // echo "12";
+  //   fwrite($myfile, $txt." : ".$json_name); // This one right here writes in the file the userID of the current user.
+  //   echo $txt." : ".$json_name;
+  // }
+  // fclose($myfile); // This one right here closes the file.
+
+  $write = $_SESSION['userID']." : ".$json_name;
+  $myfile = file_put_contents('C:\xampp\htdocs\MyCrowdSourcing\uploads\current_userID.txt', $write.PHP_EOL , FILE_APPEND | LOCK_EX);
 
   proc_close($process); // This one right here closes the process we opened before.
 }
@@ -54,7 +77,7 @@ if (isset($_POST['submit_file'])) { //This one right here checks if the user cam
 
             move_uploaded_file($file_tmp_name, $file_destination); //This one right here moves the uploaded file from temporary location to the location we want to.
 
-            execInBackground('C:/xampp/php/php.exe C:/xampp/htdocs/MyCrowdSourcing/includes/from_json_to_mysql_with_json_machine.inc.php', $_SESSION['userID']);
+            execInBackground('C:/xampp/php/php.exe C:/xampp/htdocs/MyCrowdSourcing/includes/from_json_to_mysql_with_json_machine.inc.php', $_SESSION['userID'], $file_name_new);
             // include('from_json_to_array_with_json_machine.inc.php');
             header("Location: ../index.php?upload=success");
             exit();
