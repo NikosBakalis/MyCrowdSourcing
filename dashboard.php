c83ce29f2bb21b6bcb90838b78c561aacf85fd02
NikosBakalis <bakalis.nikolas@gmail.com>
2020-08-09 13:33:21 +0300

    random_rgba() function script added!

--- dashboard.php
+++ dashboard.php
@@ -23,22 +23,22 @@ else {
     <title></title>
   </head>
   <body>
+    <script>
+      function random_rgba() {
+        var o = Math.round, r = Math.random, s = 255;
+        return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+      }
+    </script>
     <div id="div1">
       <canvas id="typePercentageChart" width="50" height="15"></canvas>
       <script>
         $.post('includes/activity_details_type_percentage.inc.php',
         function(result){
-          // console.log(result);
           var both = jQuery.parseJSON(result);
-          console.log(both);
-          console.log(Object.values(both)[0].length);
-          // var typeArray = Object.values(both)[0];
           var typeArray = [];
           for (var i = 0; i < Object.values(both)[0].length; i++) {
             typeArray.push(Object.values(both)[0][i]);
-            console.log(typeArray[i]);
           }
-          // var typeArray = jQuery.parseJSON(Object.values(both));
           var ctx = document.getElementById('typePercentageChart').getContext('2d');
           var typePercentageChart = new Chart(ctx, {
               type: 'pie',
@@ -48,30 +48,17 @@ else {
                   label: 'Percentage of each type of activity',
                   data: Object.values(both)[1], // 12, 19, 3, 5, 2, 3
                   backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(44, 99, 132, 0.2)',
-                    'rgba(23, 162, 235, 0.2)',
-                    'rgba(67, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(69, 102, 255, 0.2)'
-                  ],
-                  borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(44, 99, 132, 1)',
-                    'rgba(23, 162, 235, 1)',
-                    'rgba(67, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(69, 102, 255, 1)'
+                    random_rgba(),      // 'rgba(255, 99, 132, 0.2)',
+                    random_rgba(),      // 'rgba(54, 162, 235, 0.2)',
+                    random_rgba(),      // 'rgba(255, 206, 86, 0.2)',
+                    random_rgba(),      // 'rgba(75, 192, 192, 0.2)',
+                    random_rgba(),      // 'rgba(153, 102, 255, 0.2)',
+                    random_rgba(),      // 'rgba(255, 159, 64, 0.2)',
+                    random_rgba(),      // 'rgba(44, 99, 132, 0.2)',
+                    random_rgba(),      // 'rgba(23, 162, 235, 0.2)',
+                    random_rgba(),      // 'rgba(67, 206, 86, 0.2)',
+                    random_rgba(),      // 'rgba(75, 192, 192, 0.2)',
+                    random_rgba()       // 'rgba(69, 102, 255, 0.2)'
                   ],
                   borderWidth: 4
                 }]
@@ -90,6 +77,7 @@ else {
       </script>
     </div>
     <div id="div2">
+
     </div>
     <div id="div3">
     </div>
