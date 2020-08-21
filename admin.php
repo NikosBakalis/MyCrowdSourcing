0b33ce892f2f8491b1aa2922b6c2b70f507f1733
NikosBakalis <bakalis.nikolas@gmail.com>
2020-08-21 13:00:21 +0300

    Leaflet draw for admin!

--- admin.php
+++ admin.php
@@ -10,6 +10,8 @@
     <script src="includes/Leaflet_heat/dist/leaflet-heat.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.js"></script>
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/admin.css">
